3ac50f37ade67f8286a33f8d6fac8d6c5bf995bf
CryptoInti <cryptointi@gmail.com>
2020-04-13 21:41:33 -0400

    Exercise 3.1
    Backend with the response of the List of Persons (exercise 3.1)

---
+++ index.js
@@ -0,0 +1,34 @@
+// const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+]
+// const app = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'application/json'})
+//     res.end(JSON.stringify(persons))
+// })
+
+app.get('/', (req,res) => {
+    res.send('Hallo Phonebook Welt')
+    console.log('ask for /')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+// const port = 3001
+// app.listen(port)
+// console.log('SErver Rurnrunr')
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

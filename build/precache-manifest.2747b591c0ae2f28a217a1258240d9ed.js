76018cf4c362dec562f4fc3937ded70915e41791
CryptoInti <cryptointi@gmail.com>
2020-04-14 22:41:48 -0400

    uibuild

---
+++ build/precache-manifest.2747b591c0ae2f28a217a1258240d9ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce2fa8721e9c98794684410449fd3a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91c595051d1ac8bee521",
+    "url": "/static/css/main.5ddbd62b.chunk.css"
+  },
+  {
+    "revision": "ac7ea9b9d91b5c85465a",
+    "url": "/static/js/2.96ca01d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.96ca01d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c595051d1ac8bee521",
+    "url": "/static/js/main.dacdea43.chunk.js"
+  },
+  {
+    "revision": "d19032c1263fa01ea41a",
+    "url": "/static/js/runtime-main.08633c47.js"
+  }
+]);

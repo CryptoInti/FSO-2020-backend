bcb1371ee8bb2e24411494f659a47ad59481f568
CryptoInti <cryptointi@gmail.com>
2020-04-23 00:55:18 -0400

    Exercises Part 3

--- index.js
+++ index.js
@@ -1,108 +1,117 @@
-// const http = require('http')
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
 morgan.token('data', (req) => {
-    return JSON.stringify(req.body)
+  return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-let persons = [
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck asd', number: '39-23-6423122', id: 4 },
-    { name: 'Friedrich Nietzsche', number: '36-93-692693', id: 5 },
-]
-// const app = http.createServer((req, res) => {
-//     res.writeHead(200, {'Content-Type': 'application/json'})
-//     res.end(JSON.stringify(persons))
-// })
-
 app.get('/', (req,res) => {
-    res.send('Hallo Phonebook Welt')
-    console.log('ask for /')
-})
-
-app.get('/info', (req, res) => {
-    res.send(`Phonebook has info for ${persons.length} people <br></br>${new Date}`)
+  res.send('Hallo Phonebook Welt')
+  console.log('ask for /')
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons.map(person => person.toJSON()))
+  })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-    if(person){
-        res.json(person)
-    }else{
+app.get('/info', (req, res) => {
+  // eslint-disable-next-line no-undef
+  res.send(`Phonebook has info for ${persons.length} people <br></br>${new Date}`)
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+
+  Person.findById(req.params.id)
+    .then(person => {
+      if(person){
+        res.json(person.toJSON())
+      }else{
+        console.log('ERROR on DB when try to get a Person, the message is: ')
         res.status(404).end()
-    }
-
+      }
+    })
+    .catch((error) => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-    res.status(203).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    // eslint-disable-next-line no-unused-vars
+    .then(_result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
-const generateId = (max) => {
-    const newId = Math.floor(Math.random() * Math.floor(max))
-    const findIt = persons.find(p => p.id === newId)
-        console.log('max',max,'newId',newId,'findIt',typeof findIt)
-    if(typeof findIt !== 'object'){
-        return newId
-    }
-    else{
-        return generateId(max*2)
-    }
-}
 
 app.post('/api/persons', (req, res) => {
-    const body = req.body
-    if(!body.name || !body.number){
-        return res.status(400).json({
-            error: "name or number missing"
-        })
-    }
-
-    if(persons.find(p => p.name === body.name)){
-        res.status(400).json({
-            error: 'name must be unique'
-        })
-    }else{
-        const person = {
-            name: body.name,
-            number: body.number,
-            id: generateId(10)
-        }
+  const body = req.body
     
-        console.log(person)
-        persons = persons.concat(person)
-        res.json(person)
-    }
+  if(!body.name || !body.number){
+    return res.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+  })
+
+  person.save().then(savePerson=> {
+    res.json(savePerson.toJSON())
+  })
+    .catch((error) => {
+      console.log('Error on MongoDB when try to Save a new person, the message is: ', error.message)
+    })
+
 })
-// const port = 3001
-// app.listen(port)
-// console.log('SErver Rurnrunr')
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(updatedPerson => {
+      res.json(updatedPerson.toJSON())
+    })
+    .catch((error) => next(error))
+})
+
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send('<img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic.tumblr.com%2Fixshp06%2FYP6m2221z%2Fdennis_nedry_magic_word_header.gif&f=1&nofb=1" alt="nanana"></img>')
+  res.status(404).send('<img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic.tumblr.com%2Fixshp06%2FYP6m2221z%2Fdennis_nedry_magic_word_header.gif&f=1&nofb=1" alt="nanana"></img>')
 }
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(404).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
